83b4325ea878ffdfbc48618339d8a3f2b9316a82
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-07-14 20:57:25 +0600

    react-typescript

---
+++ src/InputFeiled/InputFeiled.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import './InputFeiled.css';
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e:React.FormEvent) => void;
+}
+
+const InputFeiled: React.FC<Props> = ({todo,setTodo, handleAdd}) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    return (
+        <form
+        className="input"
+       onSubmit={(e)=>{
+        handleAdd(e)
+        inputRef.current?.blur();
+    }}>
+        <input
+        ref={inputRef}
+          type="text"
+          placeholder="Enter a Task"
+         value={todo}
+         onChange={
+            (e)=>setTodo(e.target.value)
+         }
+        
+          className="input__box"
+        />
+        <button type="submit" className="input_submit">
+          GO
+        </button>
+      </form>
+    );
+};
+
+export default InputFeiled;
